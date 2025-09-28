bd235e43ebb52d28650574b0adad2c651e1fbd94
jeMartine <jorgeesteban464@gmail.com>
2025-09-27 22:16:12 -0500

    desing: change main window design

--- view/ventana.py
+++ view/ventana.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import os
 from PyQt5.QtWidgets import QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, QToolButton, QGroupBox, QLabel, QSizePolicy
 from view.ventana_principal_ui import Ui_MainWindow 
 from PyQt5.QtGui import QIcon
@@ -19,6 +20,23 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.showMaximized()
 
+        # base_path = os.path.dirname(os.path.abspath(__file__))
+        # ruta_logo = os.path.join(base_path, "\src\logo\logo-gammalab.png")
+
+        # print(ruta_logo)
+        # self.setWindowIcon(QIcon(ruta_logo))
+
+        # Quitar márgenes y spacing del contenedor de botones
+        self.ui.horizontalLayout_2.setContentsMargins(0, 0, 0, 10)
+        self.ui.horizontalLayout_2.setSpacing(0)
+
+        #layout principal
+        self.ui.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.ui.verticalLayout_2.setSpacing(0)
+
+        self.ui.horizontalLayout.setContentsMargins(30, 10, 30, 0)
+
+
         self.current_section = "Home"
         self.active_plugin = None #Plugin activo actualmente
         self.active_plugin_widget = None #Widget del plugin activo
@@ -29,32 +47,50 @@ class MainWindow(QMainWindow):
 
         if not self.plugin_area.layout():
             self.plugin_layout = QVBoxLayout(self.plugin_area)
-            self.plugin_layout.setContentsMargins(6, 6, 6, 6)
+            self.plugin_layout.setContentsMargins(0,0,0,0)
         else:
             self.plugin_layout = self.plugin_area.layout()
 
-        # Conectar botones de sección
-        self.ui.bnt_home.clicked.connect(lambda: self.switch_section("Home"))
-        self.ui.btn_preprocessing.clicked.connect(lambda: self.switch_section("Preprocessing"))
-        self.ui.btn_analysis.clicked.connect(lambda: self.switch_section("Analysis"))
-        self.ui.btn_measure.clicked.connect(lambda: self.switch_section("Measure"))
-        self.ui.btn_visualization.clicked.connect(lambda: self.switch_section("Visualization"))
-        self.ui.btn_utilities.clicked.connect(lambda: self.switch_section("Utilities"))
-        self.ui.btn_faq.clicked.connect(lambda: self.switch_section("FAQ"))
+        # Diccionario de secciones
+        self.section_buttons = {
+            "Home": self.ui.bnt_home,
+            "Preprocessing": self.ui.btn_preprocessing,
+            "Analysis": self.ui.btn_analysis,
+            "Measure": self.ui.btn_measure,
+            "Visualization": self.ui.btn_visualization,
+            "Utilities": self.ui.btn_utilities,
+            "FAQ": self.ui.btn_faq,
+        }
+
+        for section, btn in self.section_buttons.items():
+            btn.setCheckable(True)
+            btn.clicked.connect(lambda _, s=section: self.switch_section(s))
 
         # Mostrar por defecto plugins de Home
         self.switch_section(self.current_section)
+
 
     '''buttons section'''
     # Actualizar plugins al registrar uno nuevo
     def on_plugin_registered(self, name):
         plugin = self.kernel.get_plugin(name)
         if plugin and plugin.category() == self.current_section:
+            
             self.add_plugin_button(name)
 
     # Cambiar de sección
     def switch_section(self, section):
         self.current_section = section
+        self.setWindowTitle(f"Gamma Lab - {self.current_section}")
+
+        
+        # Desmarcar los botones y marcar el de la sección actual
+        for btn in self.section_buttons.values():
+            btn.setChecked(False)
+        if section in self.section_buttons:
+            self.section_buttons[section].setChecked(True)
+
+
         contenedor_botones = self.ui.buttonContainer.layout()
         subcategories = defaultdict(list)
 
